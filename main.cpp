8ca0942887a66252b466b516a898203380e612d6
Arnoldas Ropë <arnoldas.rope@gmail.com>
2012-10-23 18:29:58 +0200

    Nusikopijuota is Justinos

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <cstdlib>
-#include <cmath>
+#include <math.h>
 
-using namespace std;
-
+// funkcija
 double sixhump(double *x)
 {
-    double f=(4-2.1*pow(x[0],2)+pow(x[0],4)/3)*pow(x[0],2)+x[0]*x[1]+(-4+4*pow(x[1],2))*pow(x[1],2);
+    double f = (4- 2.1 * x[0] * x[0] + (pow(x[0],4))/3) * x[0] * x[0] + x[0] * x[1] + (-4+4*x[1]*x[1])*(pow(x[1],2));
     return f;
 }
+
+using namespace std;
 
 int main()
 {
@@ -22,25 +23,46 @@ int main()
 
     // Vektoriu kurimas, kuriu elementai yra atitiktiniai realus skaiciai
 
-    double virsutinis_rezis, apatinis_rezis; // apsirasome intervalo rezius
+    double virsutinis_rezis, apatinis_rezis, virsutinis_rezis2, apatinis_rezis2; // apsirasome intervalo rezius
 
-    cout << "Iveskite inervalo apatini ir virsutini rezius:" << endl;
+    cout << "Iveskite inervalo rerzius x1: ";
     cin >> apatinis_rezis >> virsutinis_rezis;
-    cout << "Iveskite vektoriaus dimensija (n):" << endl;
+    cout << "Iveskite inervalo rerzius x2: ";
+    cin >> apatinis_rezis2 >> virsutinis_rezis2;
+    cout << "Iveskite vektoriaus ilgi: ";
     cin >> n;
 
-    double vektorius[n];
-    cout<<"x = ( ";
+    double vektorius[n], funkcija[200];
+
+    for (int j=0; j<100; j++)
+    {
+
     for(int i=0; i<n; i++)
     {
-        vektorius[i] = rand() * (virsutinis_rezis - apatinis_rezis) / RAND_MAX + apatinis_rezis;
-        cout << vektorius[i]<<" "; // Vektoriaus isvedimas i ekrana
+        if (i == 0)
+        {
+           vektorius[i] = rand() * (virsutinis_rezis - apatinis_rezis) / RAND_MAX + apatinis_rezis;
+           cout << "Skaicius = " << vektorius[i] << endl; // pakeiciau
+        }
+        else
+        {
+           vektorius[i] = rand() * (virsutinis_rezis2 - apatinis_rezis2) / RAND_MAX + apatinis_rezis2;
+           cout << "Skaicius = " << vektorius[i] << endl; // pakeiciau
+        }
+
     }
-    cout<<")\n";
-    cout<<"Funkcijos sixhump reiksme taske: "<<sixhump(&vektorius[0])<<endl;
-    //pasitikrinimui funkcijos reiksme minimumo taske
-    vektorius[0]=0.089842;
-    vektorius[1]=-0.712656;
-    cout<<"Funkcijos sixhump reiksme minimumo taske: "<<sixhump(&vektorius[0])<<endl;
+
+    cout << "Funkcija = " << sixhump(&vektorius[0]) << endl; // pakeiciau
+    cout << "===================================" << endl;
+    funkcija[j] = sixhump(&vektorius[0]);
+    }
+
+    double min = funkcija[0];
+    for (int i=0; i<100; i++)
+    {
+        if (funkcija[i] < min) min = funkcija[i];
+    }
+    cout << "MIN: " << min;
+
     return 0;
 }
